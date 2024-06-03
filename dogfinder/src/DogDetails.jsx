21d36acce6f1e922375e3210f68c27f519a91cee
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-03 17:54:33 -0400

    Wrote DogDetails component

--- dogfinder/src/DogDetails.jsx
+++ dogfinder/src/DogDetails.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 /** AppComponent for summary
  *
  * Props:
- * -
+ * - Object of information for a dog like {name, age, src, facts, id}
  *
- * State:
- * -
+ * State: none
  *
- * call list
+ * App -> DogList -> DogDetails
  */
-function DogDetails() {}
 
-export default DogDetails
+function DogDetails({ dog }) {
+  return (
+    <div className="DogDetails">
+      <h2>{dog.name}</h2>
+      <img src={`../public/${dog.src}.jpg`} />
+      <p>Age: {dog.age}</p>
+      <ul>Facts about {dog.name}:
+        {dog.facts.map(
+          fact => <li key={uuid()}>{fact}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default DogDetails;
