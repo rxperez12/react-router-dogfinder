bf7f6a32bcfb6724b250ba78ea8e62ebed21f49a
Ricardo Perez <rxperez12@gmail.com>
2024-06-03 15:18:20 -0700

    added dog list function -WIP

--- dogfinder/src/DogList.jsx
+++ dogfinder/src/DogList.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { fetchDogData } from "./utils.js";
 
 /** AppComponent for summary
  *
@@ -10,6 +11,24 @@ import { useState } from "react";
  *
  * call list
  */
-function DogList() {}
+function DogList() {
+  const [dogs, setDogs] = useState("");
+  //TODO: add loading state
+
+  /** Get dogs from API and set them in state */
+  async function showsDogs() {
+    const dogs = await fetchDogData();
+
+    setDogs(dogs);
+  }
+
+  if (dogs === "") {
+    // show loading message
+  }
+
+  // List of doggos
+  return <div></div>;
+  // If clicking on doggo, take to dog detail for that dog -> put dog in url param
+}
 
 export default DogList;
