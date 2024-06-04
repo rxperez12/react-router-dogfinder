f74880bc81d9c9ad95a8d69843562ed2c899c96c
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-03 20:32:54 -0400

    Added AJAX call to API to put dog info in state.

--- dogfinder/src/App.jsx
+++ dogfinder/src/App.jsx
@@ -1,24 +1,39 @@
 import { BrowserRouter } from "react-router-dom";
+import { fetchDogsData } from "./utils.js";
+import { useState } from "react";
 import Nav from "./Nav.jsx";
 import RoutesList from "./RoutesList.jsx";
 import "./App.css";
 
-/** AppComponent for summary
+/** Main application: depending on path, show different components.
  *
- * Props:
- * -
+ * Props: none
  *
- * State:
- * -
+ * State: ?
  *
  * App -> { Nav, Routes}
  */
 function App() {
+  const [dogs, setDogs] = useState('');
+
+  /** Get dogs from API and set them in state */
+  async function getDogs() {
+    const dogs = await fetchDogsData();
+    const dogsInfo = dogs;
+    setDogs(dogs);
+  }
+
+  if (dogs === "") {
+    getDogs();
+    return <div></div>;
+  }
+
+  const dogNames = dogs.map(dog => dog.name);
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
-        <RoutesList />
+        <Nav dogNames={dogNames} />
+        <RoutesList dogs={dogs} />
       </BrowserRouter>
     </div>
   );
