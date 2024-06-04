f91d5e1fa8629a7ec321892e5c214ea0aabdf6aa
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-03 20:33:19 -0400

    Removed async function

--- dogfinder/src/DogList.jsx
+++ dogfinder/src/DogList.jsx
@@ -1,35 +1,15 @@
-import { useState } from "react";
-import { fetchDogsData } from "./utils.js";
 import { v4 as uuid } from "uuid";
 import { Link } from "react-router-dom";
 
-let dogsInfo = "";
-
-/** AppComponent for summary
+/** DogList displays name and image for all dogs.
  *
- * Props:
- * -
+ * Props: array of info for dogs like [{name, age src, facts, id}]
  *
- * State:
- * -
+ * State: none
  *
- * App -> DogList
+ * App -> Routes -> DogList
  */
-function DogList({ handleShowDogDetail }) {
-  const [dogs, setDogs] = useState("");
-
-  //TODO: move this to APP
-  /** Get dogs from API and set them in state */
-  async function showDogs() {
-    const dogs = await fetchDogsData();
-    dogsInfo = dogs;
-    setDogs(dogs);
-  }
-
-  if (dogs === "") {
-    showDogs();
-    return <div>Loading Doggos...</div>;
-  }
+function DogList({ dogs }) {
 
   return (
     <div className="DogsList">
